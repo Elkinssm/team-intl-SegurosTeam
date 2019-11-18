779818a568c8b76fb0a3812aa379f0bb420c42f4
elkinssm <elkinsilvamanrique@gmail.com>
2019-11-17 20:19:17 -0500

    Agregando helpers para implementar seguridad atraves de  jsonwebtoken

---
+++ segurosApi/teamSegurosApi/teamSegurosApi/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace teamSegurosApi.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ segurosApi/teamSegurosApi/teamSegurosApi/Helpers/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using teamSegurosApi.Data.Dto;
+
+namespace teamSegurosApi.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<UsuarioDto> WithoutPasswords(this IEnumerable<UsuarioDto> users)
+        {
+            return users.Select(x => x.WithoutPassword());
+        }
+
+        public static UsuarioDto WithoutPassword(this UsuarioDto user)
+        {
+            user.Clave = null;
+            return user;
+        }
+    }
+}
